d679d0ab734e41a7ff24737ad94a760f99e8ecc6
Mitalo <49762937+Mitalo@users.noreply.github.com>
2024-10-14 21:01:57 -0300

    correção main

--- main.py
+++ main.py
@@ -29,4 +29,24 @@ def get_config_data(config_path:str):
     configuration_file = open(config_path, 'r')
     config_data = json.load(configuration_file)
     configuration_file.close()
-    Application(config_data)
+    return config_data
+
+if __name__ == '__main__':
+    exec_args = get_execution_arguments()
+    config_path = get_config_path(exec_args)
+    config_data = get_config_data(config_path)
+    app = Application(config_data)
+
+    def signal_handler(sig, frame):
+        print('Will dispose application')
+        stop_event.set()
+
+    signal.signal(signal.SIGINT, signal_handler)
+
+    stop_event = threading.Event()
+
+    # Use a loop to poll the event status
+    while not stop_event.is_set():
+        stop_event.wait(0.1)  # Check every 100ms
+
+    app.dispose()
